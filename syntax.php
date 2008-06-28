7d064aac3dabd59436b17d23e5d8b56783dd32df
Michael Klier <chi@chimeric.de>
2008-06-28 20:34:24 +0200

    fixed broken vim modelines
    darcs-hash:20080628183424-23886-8604604b51a081ca8cf6f352fa6388a159af1e03.gz

--- syntax.php
+++ syntax.php
@@ -43,4 +43,4 @@ class syntax_plugin_comment extends DokuWiki_Syntax_Plugin {
     function handle($match, $state, $pos, &$handler){ return ''; }            
     function render($mode, &$renderer, $data) { return true; }
 }
-//vim:ts=4:sw=4:et:enc=-utf-8:
+// vim:ts=4:sw=4:et:enc=-utf-8:
