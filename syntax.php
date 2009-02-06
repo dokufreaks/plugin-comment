52986720be61f1387ae8cd4edbba2ddbcbb74ded
Michael Klier <chi@chimeric.de>
2009-02-06 20:07:34 +0100

    fixed regex
    darcs-hash:20090206190734-23886-d2dad20afbe73c6dce260c34ec167d44c968e0cf.gz

--- syntax.php
+++ syntax.php
@@ -36,11 +36,10 @@ class syntax_plugin_comment extends DokuWiki_Syntax_Plugin {
     function getSort(){ return 321; }
     
     function connectTo($mode) {
-        $this->Lexer->addSpecialPattern("/\*.*?\*/", $mode, 'plugin_comment');
-        $this->Lexer->addSpecialPattern("//.*?$", $mode, 'plugin_comment');
+        $this->Lexer->addSpecialPattern("^/\*.*?\*|\s+/\*.*?\*/", $mode, 'plugin_comment');
     }
     
     function handle($match, $state, $pos, &$handler){ return ''; }            
     function render($mode, &$renderer, $data) { return true; }
 }
-// vim:ts=4:sw=4:et:enc=-utf-8:
+// vim:ts=4:sw=4:et:enc=utf-8:
